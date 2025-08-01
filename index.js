f3a2b17863557205e46f9d5999508cebfef64d5d
sameerthakur12 <thakursameer1619@gmail.com>
2025-08-01 10:45:48 +0530

    Add route to read file content

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
+const { isUtf8 } = require('buffer');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -15,6 +16,12 @@ app.get('/', function(req, res){
         res.render('index', {files: files});
         
     });
+});
+
+app.get('/file/:filename', function(req, res){
+    fs.readFile(`./files/${req.params.filename}`, "utf-8",function(err, filedata){
+        res.render('show', {filename: req.params.filename, filedata: filedata});
+    })
 });
 
 app.post('/create', function(req, res){
