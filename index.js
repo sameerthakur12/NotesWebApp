f3560225cababb3b13a739eacab6ce9ad1c7176e
sameerthakur12 <thakursameer1619@gmail.com>
2025-08-01 11:23:03 +0530

    Add edit filename feature

--- index.js
+++ index.js
@@ -24,10 +24,22 @@ app.get('/file/:filename', function(req, res){
     })
 });
 
+app.get('/edit/:filename', function(req, res){
+    res.render('edit', {filename: req.params.filename});
+});
+
+app.post('/edit', function(req, res){
+    fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}.txt`, function (err) {
+        res.redirect(`/`);
+    });
+});
+
 app.post('/create', function(req, res){
     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function (err) {
         res.redirect("/");
     });  
 });
+
+
 
 app.listen(3000);
